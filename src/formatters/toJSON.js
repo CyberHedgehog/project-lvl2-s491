47e4bf47193f665cbfa5263aecc918c6e238480f
CyberHedgehog <reg.dobr@live.ru>
2019-07-05 10:07:40 +0300

    Names and JSON fixed

--- src/formatters/toJSON.js
+++ src/formatters/toJSON.js
@@ -1,17 +1,19 @@
 import _ from 'lodash';
 
+const valueConstructor = value => (value instanceof Object ? [value] : value);
+
 const builder = {
-  unchanged: node => ({ [`  ${node.name}`]: node.value }),
-  changed: node => ({ [`+ ${node.name}`]: node.changedTo, [`- ${node.name}`]: node.changedFrom }),
-  removed: node => ({ [`- ${node.name}`]: node.value }),
-  added: node => ({ [`+ ${node.name}`]: node.value }),
+  unchanged: node => ([{ [`  ${node.name}`]: valueConstructor(node.value) }]),
+  changed: node => ([{ [`+ ${node.name}`]: valueConstructor(node.changedTo) }, { [`- ${node.name}`]: valueConstructor(node.changedFrom) }]),
+  removed: node => ([{ [`- ${node.name}`]: valueConstructor(node.value) }]),
+  added: node => ([{ [`+ ${node.name}`]: valueConstructor(node.value) }]),
 };
 
 const formatter = (tree) => {
   const result = tree.reduce((acc, node) => {
     const { state, name, children } = node;
     if (!children) {
-      return [...acc, { ...builder[state](node) }];
+      return [...acc, ...builder[state](node, formatter)];
     }
     return [...acc, { [`  ${name}`]: formatter(children) }];
   }, []);
