66f17e134eda206a479a9da35a67c39ab577eb62
Dmitriy <reg.dobr@live.ru>
2019-07-06 12:04:49 +0300

    toJSON fixed

--- src/formatters/toJSON.js
+++ src/formatters/toJSON.js
@@ -1,24 +1,22 @@
 import _ from 'lodash';
 
-const valueConstructor = value => (value instanceof Object ? [value] : value);
+// const builder = {
+//   unchanged: node => ([{ [`  ${node.name}`]: valueConstructor(node.value) }]),
+//   changed: node => ([`+ ${node.name}`: node.changedTo, [`- ${node.name}`]: (node.changedFrom) }),
+//   removed: node => ([{ [`- ${node.name}`]: valueConstructor(node.value) }]),
+//   added: node => ([{ [`+ ${node.name}`]: valueConstructor(node.value) }]),
+// };
 
-const builder = {
-  unchanged: node => ([{ [`  ${node.name}`]: valueConstructor(node.value) }]),
-  changed: node => ([{ [`+ ${node.name}`]: valueConstructor(node.changedTo) }, { [`- ${node.name}`]: valueConstructor(node.changedFrom) }]),
-  removed: node => ([{ [`- ${node.name}`]: valueConstructor(node.value) }]),
-  added: node => ([{ [`+ ${node.name}`]: valueConstructor(node.value) }]),
-};
+// const formatter = (tree) => {
+//   const result = tree.reduce((acc, node) => {
+//     const { state, name, children } = node;
+//     if (!children) {
+//       return [...acc, ...builder[state](node, formatter)];
+//     }
+//     return [...acc, { [`  ${name}`]: formatter(children) }];
+//   }, []);
+//   return _(result).toJSON();
+// };
 
-const formatter = (tree) => {
-  const result = tree.reduce((acc, node) => {
-    const { state, name, children } = node;
-    if (!children) {
-      return [...acc, ...builder[state](node, formatter)];
-    }
-    return [...acc, { [`  ${name}`]: formatter(children) }];
-  }, []);
-  return _(result).toJSON();
-};
-
-const render = data => JSON.stringify(formatter(data));
+const render = data => JSON.stringify(_(data).toJSON());
 export default render;
